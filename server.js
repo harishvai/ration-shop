c846235f525a820d60d17b6cfad02547abaa22a7
HARISH  R RTC2025BCS152 <harishr.bcs25@rathinam.in>
2026-09-10 12:21:39 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("AI Ration Shop is running!");
+});
+
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
